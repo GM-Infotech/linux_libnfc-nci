80581ebb0eb8db80c1905ec11119c0ccfb3ebb1e
Gustav Meyer <gustav@infotech.no>
2026-02-23 13:20:14 +0100

    Add <cstdint> include and refactor struct definitions for consistency

--- src/nxp_nci_hal_libnfc-nci/src/include/Nxp_Features.h
+++ src/nxp_nci_hal_libnfc-nci/src/include/Nxp_Features.h
@@ -20,6 +20,7 @@
 #define NXP_FEATURES_H
 #include <unistd.h>
 #include <string>
+#include <cstdint>
 
 #define FW_MOBILE_MAJOR_NUMBER_PN553 0x01
 #define FW_MOBILE_MAJOR_NUMBER_PN81A 0x02
@@ -28,8 +29,9 @@
 #define FW_MOBILE_MAJOR_NUMBER_PN548AD 0x01
 /*Including T4T NFCEE by incrementing 1*/
 #define NFA_EE_MAX_EE_SUPPORTED 5
- using namespace std;
-typedef enum {
+using namespace std;
+typedef enum
+{
   unknown,
   pn547C2,
   pn65T,
@@ -43,7 +45,8 @@ typedef enum {
   pn81T
 } tNFC_chipType;
 
-typedef struct {
+typedef struct
+{
   tNFC_chipType chipType;
   std::string _FW_LIB_PATH;
   std::string _FW_BIN_PATH;
@@ -54,68 +57,82 @@ typedef struct {
 
 extern tNfc_featureList nfcFL;
 
-#define CONFIGURE_FEATURELIST(chipType)                                      \
-  {                                                                          \
-    nfcFL.chipType = chipType;                                               \
-    if (chipType == pn81T) {                                                 \
-      nfcFL.chipType = pn557;                                                \
-    } else if (chipType == pn80T) {                                          \
-      nfcFL.chipType = pn553;                                                \
-    } else if (chipType == pn67T) {                                          \
-      nfcFL.chipType = pn551;                                                \
-    } else if (chipType == pn66T) {                                          \
-      nfcFL.chipType = pn548C2;                                              \
-     }                                                                       \
-      CONFIGURE_FEATURELIST_NFCC(chipType)                                   \
+#define CONFIGURE_FEATURELIST(chipType)  \
+  {                                      \
+    nfcFL.chipType = chipType;           \
+    if (chipType == pn81T)               \
+    {                                    \
+      nfcFL.chipType = pn557;            \
+    }                                    \
+    else if (chipType == pn80T)          \
+    {                                    \
+      nfcFL.chipType = pn553;            \
+    }                                    \
+    else if (chipType == pn67T)          \
+    {                                    \
+      nfcFL.chipType = pn551;            \
+    }                                    \
+    else if (chipType == pn66T)          \
+    {                                    \
+      nfcFL.chipType = pn548C2;          \
+    }                                    \
+    CONFIGURE_FEATURELIST_NFCC(chipType) \
   }
 
-#define CONFIGURE_FEATURELIST_NFCC(chipType)                                \
-  {                                                                         \
-    nfcFL._PHDNLDNFC_USERDATA_EEPROM_OFFSET = 0x023CU;                      \
-    nfcFL._PHDNLDNFC_USERDATA_EEPROM_LEN = 0x0C80U;                         \
-                                                                            \
-    if (chipType == pn557 || chipType == pn81T) {                           \
-                                                                            \
-      STRCPY_FW_LIB("libpn7160_fw")                                          \
-      STRCPY_FW_BIN("pn7160")                                                \
-                                                                            \
-      nfcFL._FW_MOBILE_MAJOR_NUMBER = FW_MOBILE_MAJOR_NUMBER_PN557;         \
-    } else if (chipType == pn553 || chipType == pn80T) {                    \
-                                                                            \
-      STRCPY_FW_LIB("libpn553_fw")                                          \
-      STRCPY_FW_BIN("pn553")                                                \
-                                                                            \
-      nfcFL._FW_MOBILE_MAJOR_NUMBER = FW_MOBILE_MAJOR_NUMBER_PN553;         \
-                                                                            \
-    } else if (chipType == pn551 || chipType == pn67T) {                    \
-                                                                            \
-      STRCPY_FW_LIB("libpn551_fw")                                          \
-      STRCPY_FW_BIN("pn551")                                                \
-                                                                            \
-      nfcFL._PHDNLDNFC_USERDATA_EEPROM_OFFSET = 0x02BCU;                    \
-      nfcFL._PHDNLDNFC_USERDATA_EEPROM_LEN = 0x0C00U;                       \
-      nfcFL._FW_MOBILE_MAJOR_NUMBER = FW_MOBILE_MAJOR_NUMBER_PN551;         \
-                                                                            \
-    } else if (chipType == pn548C2 || chipType == pn66T) {                  \
-                                                                            \
-      STRCPY_FW_LIB("libpn548ad_fw")                                        \
-      STRCPY_FW_BIN("pn548")                                                \
-                                                                            \
-      nfcFL._PHDNLDNFC_USERDATA_EEPROM_OFFSET = 0x02BCU;                    \
-      nfcFL._PHDNLDNFC_USERDATA_EEPROM_LEN = 0x0C00U;                       \
-      nfcFL._FW_MOBILE_MAJOR_NUMBER = FW_MOBILE_MAJOR_NUMBER_PN548AD;       \
-     }                                                                      \
+#define CONFIGURE_FEATURELIST_NFCC(chipType)                          \
+  {                                                                   \
+    nfcFL._PHDNLDNFC_USERDATA_EEPROM_OFFSET = 0x023CU;                \
+    nfcFL._PHDNLDNFC_USERDATA_EEPROM_LEN = 0x0C80U;                   \
+                                                                      \
+    if (chipType == pn557 || chipType == pn81T)                       \
+    {                                                                 \
+                                                                      \
+      STRCPY_FW_LIB("libpn7160_fw")                                   \
+      STRCPY_FW_BIN("pn7160")                                         \
+                                                                      \
+      nfcFL._FW_MOBILE_MAJOR_NUMBER = FW_MOBILE_MAJOR_NUMBER_PN557;   \
+    }                                                                 \
+    else if (chipType == pn553 || chipType == pn80T)                  \
+    {                                                                 \
+                                                                      \
+      STRCPY_FW_LIB("libpn553_fw")                                    \
+      STRCPY_FW_BIN("pn553")                                          \
+                                                                      \
+      nfcFL._FW_MOBILE_MAJOR_NUMBER = FW_MOBILE_MAJOR_NUMBER_PN553;   \
+    }                                                                 \
+    else if (chipType == pn551 || chipType == pn67T)                  \
+    {                                                                 \
+                                                                      \
+      STRCPY_FW_LIB("libpn551_fw")                                    \
+      STRCPY_FW_BIN("pn551")                                          \
+                                                                      \
+      nfcFL._PHDNLDNFC_USERDATA_EEPROM_OFFSET = 0x02BCU;              \
+      nfcFL._PHDNLDNFC_USERDATA_EEPROM_LEN = 0x0C00U;                 \
+      nfcFL._FW_MOBILE_MAJOR_NUMBER = FW_MOBILE_MAJOR_NUMBER_PN551;   \
+    }                                                                 \
+    else if (chipType == pn548C2 || chipType == pn66T)                \
+    {                                                                 \
+                                                                      \
+      STRCPY_FW_LIB("libpn548ad_fw")                                  \
+      STRCPY_FW_BIN("pn548")                                          \
+                                                                      \
+      nfcFL._PHDNLDNFC_USERDATA_EEPROM_OFFSET = 0x02BCU;              \
+      nfcFL._PHDNLDNFC_USERDATA_EEPROM_LEN = 0x0C00U;                 \
+      nfcFL._FW_MOBILE_MAJOR_NUMBER = FW_MOBILE_MAJOR_NUMBER_PN548AD; \
+    }                                                                 \
   }
-#define STRCPY_FW_LIB(str) {                                                \
-  nfcFL._FW_LIB_PATH.clear();                                               \
-  nfcFL._FW_LIB_PATH.append(FW_LIB_ROOT_DIR);                               \
-  nfcFL._FW_LIB_PATH.append(str);                                           \
-  nfcFL._FW_LIB_PATH.append(FW_LIB_EXTENSION);                              \
-}
-#define STRCPY_FW_BIN(str) {                                                \
-  nfcFL._FW_BIN_PATH.clear();                                               \
-  nfcFL._FW_BIN_PATH.append(FW_BIN_ROOT_DIR);                               \
-  nfcFL._FW_BIN_PATH.append(str);                                           \
-  nfcFL._FW_BIN_PATH.append(FW_BIN_EXTENSION);                              \
-}
+#define STRCPY_FW_LIB(str)                       \
+  {                                              \
+    nfcFL._FW_LIB_PATH.clear();                  \
+    nfcFL._FW_LIB_PATH.append(FW_LIB_ROOT_DIR);  \
+    nfcFL._FW_LIB_PATH.append(str);              \
+    nfcFL._FW_LIB_PATH.append(FW_LIB_EXTENSION); \
+  }
+#define STRCPY_FW_BIN(str)                       \
+  {                                              \
+    nfcFL._FW_BIN_PATH.clear();                  \
+    nfcFL._FW_BIN_PATH.append(FW_BIN_ROOT_DIR);  \
+    nfcFL._FW_BIN_PATH.append(str);              \
+    nfcFL._FW_BIN_PATH.append(FW_BIN_EXTENSION); \
+  }
 #endif
